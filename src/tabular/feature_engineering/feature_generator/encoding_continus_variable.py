4aab3eb185a65d1f20b6e923fbdc3f1908cd2b2a
rwbfd <ran.wang.math@gmail.com>
2020-02-06 12:04:08 +0800

    cotinuous variable encoder add distribution check

--- src/tabular/feature_engineering/feature_generator/encoding_continus_variable.py
+++ src/tabular/feature_engineering/feature_generator/encoding_continus_variable.py
@@ -1,8 +1,18 @@
 import numpy as np
 import warnings
-from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler, Normalizer, RobustScaler, PowerTransformer,StandardScaler
+from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler, Normalizer, RobustScaler, PowerTransformer, StandardScaler
 from scipy.special import erfinv
-from scipy.stats import norm
+from scipy.stats import kstest, norm
+
+
+def check_distribution(input_df):
+    distribution_types = ["norm", "uniform"]
+    for distribution_type in distribution_types:
+        statistic, p_value = kstest(input_df, distribution_type)
+        if p_value >= 0.05:
+            return distribution_type
+
+    return "others"
 
 
 class GaussRankScaler():
@@ -39,36 +49,52 @@ def encode_continuous_variable(df, configgers):
     df_t: pd.DataFrame, the DataFrame after transform, the result columns after transform named like "{origin_column_name}_{method}"
     """
     df_t = df
+
     for configger in configgers:
         encode_col = configger.encode_col
         method = configger.method
 
-        if method == "MinMax":
-            res = MinMaxScaler().fit_transform(df[[encode_col]])
+        distribution_type = check_distribution(df[[encode_col]])
+        if distribution_type == "uniform":
+            if method == "MinMax":
+                res = MinMaxScaler().fit_transform(df[[encode_col]])
 
-        elif method == "MinAbs":
-            res = MaxAbsScaler().fit_transform(df[[encode_col]])
+            elif method == "MinAbs":
+                res = MaxAbsScaler().fit_transform(df[[encode_col]])
 
-        elif method == "Normalize":
-            res = Normalizer().fit_transform(df[[encode_col]])
+            elif method == "Normalize":
+                res = Normalizer().fit_transform(df[[encode_col]])
 
-        elif method == "Robust":
-            res = RobustScaler().fit_transform(df[[encode_col]])
+            elif method == "Robust":
+                res = RobustScaler().fit_transform(df[[encode_col]])
 
-        elif method == "BoxCox":
-            res = PowerTransformer(method="box-cox").fit_transform(df[[encode_col]])
+            else:
+                raise ValueError(
+                    """The column '{}' most likely a uniformly distribution. So, the method value must be in ["MinMax", "MinAbs", "Normalize", "Robust"]""".format(
+                        encode_col))
 
-        elif method == "Yeo-Johnson":
-            res = PowerTransformer(method="yeo-johnson").fit_transform(df[[encode_col]])
+        elif distribution_type == "norm":
+            if method == "BoxCox":
+                res = PowerTransformer(method="box-cox").fit_transform(df[[encode_col]])
 
-        elif method == "RankGuass":
-            res = GaussRankScaler().fit_transform(df[[encode_col]])
+            elif method == "Yeo-Johnson":
+                res = PowerTransformer(method="yeo-johnson").fit_transform(df[[encode_col]])
 
-        elif method == "ICDF":
-            res = norm.ppf(df[[encode_col]])
+            elif method == "RankGuass":
+                res = GaussRankScaler().fit_transform(df[[encode_col]])
+            else:
+                raise ValueError(
+                    """The column '{}' most likely a normal distribution. So, the method value must be in ["BoxCox", "Yeo-Johnson", "Normalize"]""".format(
+                        encode_col))
+
         else:
-            raise ValueError("""The method value {func} is not be support. It must be in ["MinMax", "MinAbs", "Normalize", "Robust", "BoxCox", "Yeo-Johnson", "RankGuass", "ICDF"]""".format(func=method))
+            if method == "ICDF":
+                res = norm.ppf(df[[encode_col]])
+            else:
+                raise ValueError(
+                    """The column '{}' maybe is others distribution. So, the method value must be in ["BoxCox", "Yeo-Johnson", "Normalize"]""".format(
+                        encode_col))
 
-        df_t.loc[:,"_".join([encode_col,method])] = res
+        df_t.loc[:, "_".join([encode_col, method])] = res
 
     return df_t
